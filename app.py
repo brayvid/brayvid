ac7d5764379b83ee3b2b470857682e87c85a1298
Blake Rayvid <brayvid@users.noreply.github.com>
2025-08-26 14:47:47 -0400

    favicon route

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'src/favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/')
 def resume():
     """Renders the HTML resume page."""
